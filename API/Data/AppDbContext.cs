dc00331fc1157d33ca97a551e8ba0dddef43e075
DannyTM12 <carlosdtm8@gmail.com>
2025-10-30 03:45:37 -0600

    Parcial02 - Add Member Migration

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
-using Microsoft.EntityFrameworkCore;
 using API.Entities;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<AppUser> Users { get; set; }
+  public DbSet<AppUser> Users { get; set; }
+  public DbSet<Member> Members { get; set; }
+  public DbSet<Photo> Photos { get; set; }
 }
