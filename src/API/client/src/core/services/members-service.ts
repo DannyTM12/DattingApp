e73792a9e936766ad6b6493268d6b4a097eef09f
Daniel Torres <carlosdtm8@gmail.com>
2026-03-04 13:56:04 -0600

    Parcial04 - Add main photo selection

--- src/API/client/src/core/services/members-service.ts
+++ src/API/client/src/core/services/members-service.ts
@@ -32,4 +32,14 @@ export class MembersService {
   updateMember(member: EditableMember) {
     return this.http.put(this.baseUrl + "members", member);
   }
+
+  uploadPhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/photo', formData);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/photo/' + photo.id, {});
+  }
 }
