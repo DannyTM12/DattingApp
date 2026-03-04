69c2093ef51bdb8f431a53fdccb0096f99ff3bc9
Daniel Torres <carlosdtm8@gmail.com>
2026-03-04 13:52:55 -0600

    Parcial04 - Add photo upload feature

--- src/API/client/src/core/services/busy-service.ts
+++ src/API/client/src/core/services/busy-service.ts
@@ -1,16 +1,41 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { EditableMember, Member, Photo } from '../../types/member';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BusyService {
-  busyRequestCount = signal(0);
+export class MembersService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  editMode = signal(false);
+  member = signal<Member | null>(null);
 
-  busy() {
-    this.busyRequestCount.update(current => current + 1);
+  getMember(id: string): Observable<Member> {
+    return this.http.get<Member>(this.baseUrl + "members/" + id).pipe(
+      tap(member => {
+        this.member.set(member);
+      })
+    );
   }
 
-  idle() {
-    this.busyRequestCount.update(current => Math.max(0, current - 1));
+  getMembers(): Observable<Member[]> {
+    return this.http.get<Member[]>(this.baseUrl + "members");
+  }
+
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
+  }
+
+  updateMember(member: EditableMember) {
+    return this.http.put(this.baseUrl + "members", member);
+  }
+
+  uploadPhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/photo', formData);
   }
 }
